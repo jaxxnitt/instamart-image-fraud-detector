731cff307a18af3b289eecc042ce19b267743c09
jaxxnitt <139496135+jaxxnitt@users.noreply.github.com>
2025-11-30 11:39:01 +0530

    Enable CORS for frontend

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI, UploadFile, File, HTTPException
 from fastapi.responses import JSONResponse
 from detector import analyze_image_bytes
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(
     title="Instamart AI Image Fraud Detector (Prototype)",
     version="0.1.0",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins (or restrict later)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
